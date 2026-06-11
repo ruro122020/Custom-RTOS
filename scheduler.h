1706953e57231a0620fb7068723d6dba5ca14381
ruth <ruro122020@gmail.com>
2026-06-11 17:48:08 -0400

    added include guard and public API declaration to scheduler.h

--- scheduler.h
+++ scheduler.h
@@ -1,3 +1,5 @@
+#ifndef SCHEDULER_H
+#define SCHEDULER_H
 #include <stdint.h> /* gives us uint8_t, uint16_t, etc. — fixed-size integers */
 
 #define MAX_TASKS 2
@@ -8,3 +10,17 @@ typedef struct
 } task_control_block_t;
 
 typedef void (*task_func_t)(void);
+
+extern task_control_block_t os_tasks[MAX_TASKS];
+
+extern volatile uint8_t os_current_task;
+
+extern volatile uint8_t os_task_count;
+
+void os_init(void);
+
+int8_t os_task_create(task_func_t function);
+
+void os_start(void);
+
+#endif
